251cd21b9bcbbd5dac08767a13b3e1d70995ba2f
srbm <srbmwins@gmail.com>
2016-03-07 20:07:15 -0500

    add twitter api and a contact form. started JS for capturing form

---
+++ js/form.js
@@ -0,0 +1,13 @@
+//Take text from form 
+	//store it in an array
+
+// and send it to my email
+
+//
+var contactUS = [];
+
+var person = document.getElementById('person').value; //get the name from text field
+var personEmail; //get email form text field
+var comments; //get comments from text field
+
+
--- js/play.js
+++
@@ -1,2 +0,0 @@
-//Hover over play-type div displays link lists
-//click on link displays info overlayed current site includes links to external sites
