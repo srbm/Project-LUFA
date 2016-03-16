d333291c08ebde1fd204d9ff96bc1b63424c6961
srbm <srbmwins@gmail.com>
2016-03-15 22:04:06 -0400

    cleaned up twitter API, comment out some angular ideas

--- js/play.js
+++ js/play.js
@@ -1,9 +1,4 @@
 
-	
-
-//Problem: Create interactivity on main page to fulfill main JS requirment for project
-//Solution: Hover or click events trigger modal displaying options with links to go to change modal 
-
 //Remove titles, leave list of options, then in JS hide lists and append button title on top.
 	//Hide ul links
 $(".links").hide();
@@ -27,16 +22,18 @@ $("button.menu").click(function (event) {
 	$(this).hide();
 });
 
+//on home/banner click
+/* $(".lufa-banner").click(function (event) {
+	//stop home page loading
+	event.preventDefault();
+//give banner animation
+	$(".lufa-banner").slide down and out toward screen
+//load home page
+	$(document).load('BSindex.html');
+});
+*/
 
 
 
 
-//Display specific modal according to the choice clicked with more options to click on
-//Capture click/tap event on an option
-
-//change modal to the option tapped/clicked
-	//give a back option
-
-//provide an X at upper right to return to exit modal
-	//exit can be achieved by esc as well
 
